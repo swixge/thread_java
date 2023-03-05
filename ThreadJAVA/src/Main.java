66949c501aeacde13d22878871897f31bf5c8cb6
Vlad <71086815+swixge@users.noreply.github.com>
2023-03-05 10:21:26 +0200

    java_dona

--- ThreadJAVA/src/Main.java
+++ ThreadJAVA/src/Main.java
@@ -1,55 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Vvedit` krok: ");
-        int step = scanner.nextInt();   //крок
+        int step = scanner.nextInt();
         System.out.print("Vvedit` kilkist` potokiv: ");
-        byte numThreads = scanner.nextByte();     //кількість потоків
-        double[] sums = new double[numThreads];   // сума кожного з потоків
-        double[] counts = new double[numThreads];   //кількість доданків
-        boolean canStop = false;
+        int numThreads = scanner.nextInt();
+        int permissionInterval = 3000;
 
-        Thread[] threads = new Thread[numThreads];
+        SummingThread[] threads = new SummingThread[numThreads];
+
         for (int i = 0; i < numThreads; i++) {
-            int index = i;  //позицію потоку в масиві
-            threads[i] = new Thread(() -> calculator(index, step, sums, counts, canStop));
+            threads[i] = new SummingThread(i, step);
             threads[i].start();
         }
 
-        Thread stopperThread = new Thread(() -> {
-            try {
-                stopper(canStop);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        stopperThread.start();
-
-
-        stopperThread.join();
+        try {
+            Thread.sleep(permissionInterval);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         for (int i = 0; i < numThreads; i++) {
-            System.out.println("Thread " + (i + 1) + ": sum = " + sums[i] + " count = " + counts[i]);
+            threads[i].setRunning(false);
         }
     }
 
-    static void calculator(int index, int step, double[] sums, double[] counts, boolean canStop) {
-        double sum = 0;
-        int count = 0;
+    private static class SummingThread extends Thread {
+        private final int id;
+        private final int step;
+        private volatile boolean running = true;
 
-        for (int i = 0; !canStop; i += step) {
-            sum += i;
-            count++;
+        public SummingThread(int id, int step) {
+            this.id = id;
+            this.step = step;
         }
 
-        sums[index] = sum;
-        counts[index] = count;
-    }
+        public void run() {
+            double sum = 0;
+            int count = 0;
+            double current = 0;
 
-    static void stopper(boolean canStop) throws InterruptedException {
-        Thread.sleep(5000);
-        canStop = true;
+            while (running) {
+                sum += current;
+                count++;
+                current += step;
+            }
+
+            System.out.printf("Thread %d: sum=%f count=%d\n", id + 1, sum, count);
+        }
+
+        public void setRunning(boolean running) {
+            this.running = running;
+        }
     }
 }
